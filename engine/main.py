0ffb3ac6f3969deb71fe150b15d054515e6b467a
MarcelWinterot <113690851+MarcelWinterot@users.noreply.github.com>
2023-04-17 22:29:08 +0200

    Update as of 17.04.2023
    Fixed evaluation and added points for mobility of pieces

--- engine/main.py
+++ engine/main.py
@@ -6,19 +6,37 @@ pieceValues = {
     "p": -1, "n": -3, "b": -3, "r": -5, "q": -9, "k": -100_000
 }
 
-def hangingPieces(ply, board,currentValue, values=pieceValues):
+def hangingPieces(ply, board, currentValue, values=pieceValues):
     if ply == 0:
         return currentValue
 
     for move in board.legal_moves:
         capturedPiece = board.piece_at(move.to_square)
         if capturedPiece:
-            currentValue -= values[capturedPiece.symbol()]
-            global hypotheticalBoard;
-            hypotheticalBoard = board.copy()
-            hypotheticalBoard.push(move)
+            capturingPiece = board.piece_at(move.from_square)
+            if capturingPiece:
+                safetyValue = 1 if board.is_attacked_by(not board.turn, move.to_square) else 0
+                captureValue = values[capturedPiece.symbol()]
+                currentValue += captureValue - safetyValue * captureValue
+                hypotheticalBoard = board.copy()
+                hypotheticalBoard.remove_piece_at(move.to_square)
+                currentValue = hangingPieces(ply-1, hypotheticalBoard, currentValue)
 
-    return hangingPieces(ply-1, hypotheticalBoard, currentValue)
+    return currentValue
+
+
+def mobility(board, multiplier = 0.1):
+    wM = 0
+    for _ in board.legal_moves:
+        wM += 1
+
+    board.turn = not board.turn 
+    bM = 0
+    for _ in board.legal_moves:
+        bM += 1
+
+    board.turn = not board.turn 
+    return (wM - bM) * multiplier
 
 def evaluate(board, values=pieceValues):
     value = 0
@@ -31,6 +49,9 @@ def evaluate(board, values=pieceValues):
 
     # Hanging pieces
     value += hangingPieces(2, board, 0)
+
+    # Mobility
+    value += mobility(board)
 
     return value
 
@@ -62,6 +83,14 @@ def minMax(board, depth, bestMove, bestScore, maximingPlayer):
 
 """
 TODO:
-1. Fix evaluation as hangingPieces isn't working correctly
-2. Finish minMax
+1. Add pawn structure to evaluate (don't know if I will do it)
+2. Finish minMax -- Working on
+3. Change minMax to alpha beta
+4. Add move ordering
+5. Convert this into an api
+6. Make a web application for it that sends requests to api
+
+DONE:
+1. Fixed hangingPieces - 17.04.2023
+2. Added mobility to evaluate function - 17.04.2023
 """
