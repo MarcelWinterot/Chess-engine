942e18b56b14511158461f86db579c23c4dfb518
MarcelWinterot <u66_marwin_waw@technischools.com>
2023-07-15 16:47:25 +0200

    transposition working but eats 100% memory

--- sourceCode/main.cpp
+++ sourceCode/main.cpp
@@ -4,27 +4,40 @@
 #include "thc.h"
 #include <unordered_map>
 
-struct HashEntry {
-    std::string fen;
-    short evaluation{};
-    short depth{};
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+
+struct hashEntry{
+    char depth;
+    short eval;
 };
 
-short findEvaluation(const std::unordered_map<long long, HashEntry>& hashMap, const std::string& fen, short minDepth){
-    for (const auto& entry : hashMap) {
-        if (entry.second.fen == fen && entry.second.depth >= minDepth) {
-            return entry.second.evaluation;
-        }
+const unsigned long long amountOfRows = 5000000000;
+std::vector<hashEntry> transpositionTable(amountOfRows);
+
+/*
+Stores the given hash into the transposition table
+ */
+void storeHashEntry(thc::ChessRules& cr, char depth, short eval){
+    uint64_t hash = cr.HashCalculate();
+    hashEntry entry = {depth, eval};
+    transpositionTable[hash] = entry;
+}
+
+/*
+Returns the evaluation of the given hash
+ */
+short getEvalFromHash(uint64_t hash, char minDepth){
+    hashEntry result = transpositionTable[hash];
+    if (result.depth >= minDepth && result.depth > 4){
+        return result.eval;
     }
     return 404;
 }
 
-void insertEntry(std::unordered_map<long long, HashEntry>& hashMap, const std::string& fen, short evaluation, short depth) {
-    long long newKey = static_cast<long long>(hashMap.size()) + 1;
-    HashEntry newEntry = {fen,evaluation, depth};
-    hashMap[newKey] = newEntry;
-}
-
+/*
+Counts material on the board and returns the score
+ */
 short evaluateMaterial(const thc::ChessRules& board) {
     short score = 0;
     static const std::unordered_map<char, short> pieceValues = {
@@ -32,7 +45,7 @@ short evaluateMaterial(const thc::ChessRules& board) {
             {'p', -1}, {'n', -3}, {'b', -3}, {'r', -5}, {'q', -9}
     };
 
-    for (int i = 0; i < 64; i++) {
+    for (short i = 0; i < 64; i++) {
         auto square = static_cast<thc::Square>(i);
         char piece = board.squares[square];
         auto it = pieceValues.find(piece);
@@ -44,17 +57,21 @@ short evaluateMaterial(const thc::ChessRules& board) {
     return score;
 }
 
-
-
+/*
+Displays the current position in the console
+ */
 void DisplayPosition( thc::ChessRules &cr)
 {
     std::string s = cr.ToDebugStr();
     std::cout << s.c_str() << std::endl;
 }
 
+/*
+Returns a vector of legal moves sorted by checks, captures and normal moves
+ */
 std::vector<thc::Move> GetLegalMoves( thc::ChessRules &cr )
 {
-    std::vector<thc::Move> moves, checks, captures, attacks;
+    std::vector<thc::Move> moves, checks, captures, normal;
     std::vector<bool> check, mate, stalemate;
     cr.GenLegalMoveList(  moves, check, mate, stalemate );
     for (int i = 0; i < moves.size(); i++) {
@@ -63,17 +80,20 @@ std::vector<thc::Move> GetLegalMoves( thc::ChessRules &cr )
         } else if (cr.squares[moves[i].dst] != ' ') {
             captures.push_back(moves[i]);
         } else {
-            attacks.push_back(moves[i]);
+            normal.push_back(moves[i]);
         }
     }
     moves.clear();
     moves.insert(moves.end(), checks.begin(), checks.end());
     moves.insert(moves.end(), captures.begin(), captures.end());
-    moves.insert(moves.end(), attacks.begin(), attacks.end());
+    moves.insert(moves.end(), normal.begin(), normal.end());
     return moves;
 }
 
-short alphaBeta(thc::ChessRules &cr, short depth, short alpha, short beta, bool maximizingPlayer, std::unordered_map<long long, HashEntry> &hashMap){
+/*
+Returns the evaluation of the given position
+ */
+short alphaBeta(thc::ChessRules &cr, short depth, short alpha, short beta, bool maximizingPlayer){
     if (depth == 0){
         return evaluateMaterial(cr);
     }
@@ -83,11 +103,14 @@ short alphaBeta(thc::ChessRules &cr, short depth, short alpha, short beta, bool 
         for (auto &move : GetLegalMoves(cr)) {
             thc::ChessRules cr_copy = cr;
             cr_copy.PlayMove(move);
-            short eval = findEvaluation(hashMap, cr_copy.ForsythPublish(), depth - 1);
-            if (eval == 404){
-                eval = alphaBeta(cr_copy, depth - 1, alpha, beta, false, hashMap);
-                insertEntry(hashMap, cr_copy.ForsythPublish(), eval, depth - 1);
+            long long hash = cr_copy.HashCalculate();
+            short eval = getEvalFromHash(hash, depth - 1);
+
+            if (eval==404){
+                eval = alphaBeta(cr_copy, depth - 1, alpha, beta, false);
+                storeHashEntry(cr_copy, depth - 1, eval);
             }
+
             maxEval = std::max<short>(maxEval, eval);
             alpha = std::max<short>(alpha, eval);
             if (beta <= alpha){
@@ -100,11 +123,15 @@ short alphaBeta(thc::ChessRules &cr, short depth, short alpha, short beta, bool 
         for (auto &move : GetLegalMoves(cr)) {
             thc::ChessRules cr_copy = cr;
             cr_copy.PlayMove(move);
-            short eval = findEvaluation(hashMap, cr_copy.ForsythPublish(), depth - 1);
-            if (eval == 404){
-                eval = alphaBeta(cr_copy, depth - 1, alpha, beta, true, hashMap);
-                insertEntry(hashMap, cr_copy.ForsythPublish(), eval, depth - 1);
+            long long hash = cr_copy.HashCalculate();
+
+            short eval = getEvalFromHash(hash, depth - 1);
+
+            if (eval==404){
+                eval = alphaBeta(cr_copy, depth - 1, alpha, beta, true);
+                storeHashEntry(cr_copy, depth - 1, eval);
             }
+
             minEval = std::min<short>(minEval, eval);
             beta = std::min<short>(beta, eval);
             if (beta <= alpha){
@@ -115,46 +142,45 @@ short alphaBeta(thc::ChessRules &cr, short depth, short alpha, short beta, bool 
     }
 }
 
-thc::Move findBestMove(thc::ChessRules &cr, short depth, bool isWhite, std::unordered_map<long long, HashEntry>& hashMap){
+/*
+Returns the best move for the given position
+ */
+thc::Move findBestMove(thc::ChessRules &cr, short depth, bool isWhite){
     thc::Move bestMove = thc::Move();
 
     short bestValue = isWhite ? -1000 : 1000;
 
+    std::cout << "Starting search\n";
     for (auto &move : GetLegalMoves(cr)) {
         thc::ChessRules cr_copy = cr;
         cr_copy.PlayMove(move);
+        uint64_t hash = cr_copy.HashCalculate();
 
-        std::string fen = cr_copy.ForsythPublish();
-        short eval = findEvaluation(hashMap, fen, depth);
-
-        if (eval == 404){
-            eval = alphaBeta(cr_copy, depth - 1, -1000, 1000, !isWhite, hashMap);
-            insertEntry(hashMap, fen, eval, depth - 1);
+        short eval = getEvalFromHash(hash, depth - 1);
+        if (eval==404){
+            eval = alphaBeta(cr_copy, depth - 1, -1000, 1000, !isWhite);
+            storeHashEntry(cr_copy, depth - 1, eval);
         }
-
-        std::cout << move.NaturalOut(&cr).c_str() << ": " << eval << "\n";
 
         if((isWhite && eval > bestValue) || (!isWhite && eval < bestValue)){
             bestValue = eval;
             bestMove = move;
         }
+        std::cout << "Move: " << move.NaturalOut(&cr).c_str() << ", eval" << eval << "\n";
     }
     std::string mv_txt = bestMove.NaturalOut(&cr);
-    std::cout << "Move: " << mv_txt.c_str() << ", Eval" << ((isWhite) ? " Max: " : " Min: ") << bestValue << "\n";
+    std::cout << "Move: " << mv_txt.c_str() << ", Eval" << bestValue << "\n";
 
     return bestMove;
 }
-
-
 
 int main()
 {
     thc::ChessRules cr;
     cr.Forsyth("r3kb1r/pp3pp1/1np1pn1p/4N2P/2PP1B2/6N1/qP2QPP1/2KRR3 w kq - 0 2");
-    std::unordered_map<long long, HashEntry> hashMap;
-    hashMap.clear();
-    std::cout << hashMap.size() << std::endl;
-    findBestMove(cr, 8, true, hashMap);
+    transpositionTable.reserve(amountOfRows);
+    DisplayPosition(cr);
+    findBestMove(cr, 8, true);
     return 0;
 }
 
@@ -164,3 +190,5 @@ int main()
 // 3. Add quiescence search
 // 4. Add opening book
 // 5. Add endgame table bases
+
+#pragma clang diagnostic pop
